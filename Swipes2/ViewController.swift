4a155be04f31d6356cfb59b95dcf0603ebfcd098
user148189 <user148189@74-80-245-25.ic2net.net>
2019-04-08 22:55:47 -0700

    First Draft

--- Swipes2/ViewController.swift
+++ Swipes2/ViewController.swift
@@ -2,19 +2,42 @@
 //  ViewController.swift
 //  Swipes2
 //
-//  Created by user148189 on 4/8/19.
+//  Created by esanchez on 4/8/19.
 //  Copyright © 2019 Tec. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var label: UILabel!
+    
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let vertical = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.reportVerticalSwipe(_:)))
+        vertical.direction = [.up, .down]
+        view.addGestureRecognizer(vertical)
+        
+        let horizontal = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.reportHorizontalSwipe(_:)))
+        horizontal.direction = [.left, .right]
+        view.addGestureRecognizer(horizontal)
     }
-
-
-}
+    
+    @objc func reportHorizontalSwipe(_ recognizer:UIGestureRecognizer) {
+        label.text = "Horizontal swipe detected"
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            self.label.text = ""
+        }
+    }
+    
+    @objc func reportVerticalSwipe(_ recognizer:UIGestureRecognizer) {
+        label.text = "Vertical swipe detected"
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            self.label.text = ""
+        }
+    }}
 
